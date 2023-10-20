0a0c4cae3ea38807db4bb1f3f721da6654292d0e
HalienCoder <nihaal.sivapuram@gmail.com>
2023-10-21 03:15:46 +0530

    added new font, about me section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     extend: {
       fontFamily: {
         Roboto: ["Roboto Slab", "serif"],
+        Coffee: ["Coffee Healing", "cursive"],
+        Milocha: ["Milocha", "cursive"],
+      },
+      colors: {
+        LiBlue: "#A8E8F9",
+        Blu: "00537A",
+        DaBlu: "#013C58",
+        DaYello: "#F5A201",
+        Yello: "#FFBA42",
+        LiYello: "FFD35B",
       },
     },
   },
